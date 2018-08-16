eef9e787d7c90c541daa66e6fdf6b156332e96e9
rairai214 <spt85wx9@gmail.com>
2018-08-16 15:51:54 +0900

    first commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    private final String firstName;
+    private final String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+}
